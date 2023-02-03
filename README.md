2f607eaef0159c92fa7771a935cb209341b4c685
Balázs Réka <rekabalazs@ktch.hu>
2023-02-03 12:37:31 +0100

    mezők felirata javítva

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 #330-as Feladat megoldása
 Node js projekt
+
+Egy gömb térfogatának és felszínének számítása a gömb sugarának cm-ben megadott mérete alapján.
+
+A gömb térfogatának kiszámítása a következő képlettel történik:
+V={4 r^3 pi }/3
+
+A gömb felszínének kiszámítása a következő képlettel történik:
+A=4 pi r^2
+
--- src/gombter.js
+++ src/gombter.js
@@ -1,3 +1,18 @@
 function terfogat() {
   alert('Szamolom');
 }
+//File: index.html
+//Author: Balázs Réka
+//Copyright: 2023, Balázs Réka
+//Group: Szoft 1-2-E
+//Date: 2023-02-03
+//Github: https://github.com/BalazsR2022/Beadando
+
+
+var r = input sugar;
+function gombTerfogata(sugar) {
+  document.getElementById("szoveg").innerHTML ={4 r^3 pi }/3;
+  }
+function gombFelszíne(sugar) {
+  document.getElementById("szoveg").innerHTML =4 pi r^2;
+  }
