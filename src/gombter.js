c5b69ba37f55c8e693526cd7390acf7964ac2d55
Balázs Réka <rekabalazs@ktch.hu>
2023-02-03 14:05:57 +0100

    változók javítva

--- src/gombter.js
+++ src/gombter.js
@@ -8,11 +8,28 @@ function terfogat() {
 //Date: 2023-02-03
 //Github: https://github.com/BalazsR2022/Beadando
 
+const form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
 
-var r = input sugar;
-function gombTerfogata(sugar) {
-  document.getElementById("szoveg").innerHTML ={4 r^3 pi }/3;
-  }
-function gombFelszíne(sugar) {
-  document.getElementById("szoveg").innerHTML =4 pi r^2;
-  }
+  
+const sugar = document.querySelector('.form-control');
+const r = parseFloat(sugar.value);
+const Terfogat = szamitTerfogat(r);
+const Felszin = szamitFelszin(r);
+
+const pi = Math.PI;
+
+function szamitTerfogat(r) {
+  return (4/3) * pi * Math.pow(r, 3);
+}
+
+function szamitFelszin(r) {
+  return 4 * pi * Math.pow(r, 2);
+}
+
+
+
+
+console.log(`A gömb térfogata: ${Terfogat} cm^3`);
+console.log(`A gömb felszíne: ${Felszin} cm^2`);
